49f80294555fd17f7bb3a5eed277cf0bf1b48f20
paperd <chiloong@vslashr.com>
2021-06-14 18:02:57 +0800

    Major updates to new version. 13th June

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
 let selection = "date";
 let selectedCase = {"id": "-", "age": "-", "gender": "-", "nationality": "-", "occupation": "-", "organization": "-", "date": "-", "vaccinated": "-"};
+let caseResult = [];
+let searchString = "";
 let dateFormat = d3.timeParse("%d/%m/%Y");
 let dateScale = d3.scaleLinear()
-  .domain([dateFormat("14/05/2021"), dateFormat("28/05/2021"), dateFormat("11/06/2021")])
+  .domain([dateFormat("16/05/2021"), dateFormat("30/05/2021"), dateFormat("13/06/2021")])
   .range(["#aaa", "#ff0", "#f00"]);
 
 let ageScale = d3.scaleQuantize([0, 90], d3.schemeRdBu[9]);
 let genderScale = d3.scaleOrdinal(["male", "female"], ["steelblue", "pink"]);
-let vaccineScale = d3.scaleOrdinal(["-", "partial (1 dose)", "yes (2 doses)"], ["#aaa", "yellow", "green"]);
+let vaccinatedScale = d3.scaleOrdinal(["-", "partial (1 dose)", "yes (2 doses)"], ["#aaa", "yellow", "green"]);
 let asymptomaticScale = d3.scaleOrdinal(["-", "yes"], ["#aaa", "blueviolet"]);
 
 Promise.all([d3.json("data/links.json"), d3.json("data/cases.json"), d3.json("data/MOHlinks.json")]).then(data => {
@@ -17,14 +19,11 @@ Promise.all([d3.json("data/links.json"), d3.json("data/cases.json"), d3.json("da
         e.target = e.infectee;
     });
     
-    //console.log(data[0]);
-    //console.log(data[1]);
-    //console.log(data[2]);
-    //console.log(data[1].filter(d => d.variant != null));
+caseResult = data[1];
 
 d3.select("#casecount").text(d3.timeFormat("%d %b %Y, %a")(dateFormat(data[2][0].date)) + " (" + (data[1].length - data[1].filter(d => d.bigcluster == true).length) + " cases)");
 
-let width = 1800,
+let width = 2000,
     height = 1800;
 
 let force = d3.forceSimulation(data[1])
@@ -47,15 +46,15 @@ svg.append("rect")
     .attr("width", width)
     .attr("height", height);
     
-let chart = svg.append("g").attr("id", "chart");
+let graph = svg.append("g").attr("id", "graph");
     
-let linkpath = chart.append("g").attr("id", "links")
+let linkpath = graph.append("g").attr("id", "links")
     .selectAll("path")
     .data(data[0])
   .enter().append("path")
     .attr("class", "link");
 
-let nodes = chart.append("g").attr("id", "nodes")
+let nodes = graph.append("g").attr("id", "nodes")
     .selectAll("g")
     .data(data[1])
   .enter()
@@ -73,8 +72,8 @@ let circle = nodes.append("circle")
             return ageScale(d.age);
         } else if (selection == "gender") {
             return genderScale(d.gender);
-        } else if (selection == "vaccine") {
-            return vaccineScale(d.vaccinated);
+        } else if (selection == "vaccinated") {
+            return vaccinatedScale(d.vaccinated);
         } else if (selection == "asymptomatic") {
             return asymptomaticScale(d.asymptomatic);
         }
@@ -129,11 +128,13 @@ let circle = nodes.append("circle")
     .on("click", (event,d) => {
         d3.selectAll(".node")
         .classed("clicked", false);
-        
-        console.log("clicked", d);
 
         if (d.id == selectedCase.id) {
             selectedCase = {"id": "-", "age": "-", "gender": "-", "nationality": "-", "occupation": "-", "organization": "-", "date": "-", "vaccinated": "-"};
+            caseResult = data[1];
+            searchString = "";
+            d3.select("#searchField").node().value = "";
+
         } else {
             selectedCase = d;
             
@@ -145,10 +146,16 @@ let circle = nodes.append("circle")
                     return "<a href='" + MOHdate.link + "' target='_blank'>" + d3.timeFormat("%d %b %Y, %a")(dateFormat(selectedCase.date)) + "</a>";
                 }
             });
-            
+
+            caseResult = [selectedCase];
+            searchString = selectedCase.id;
+            d3.select("#searchField").node().value = selectedCase.id;
+
             d3.select(event.currentTarget)
             .classed("clicked", true);
         }
+
+        drawChart(selection, caseResult);
     })
     .call(d3.drag()
         .on("start", dragstarted)
@@ -170,13 +177,15 @@ svg.call(
     d3.zoom()
         .scaleExtent([.5, 4])
         .on("zoom", event => { 
-            chart.attr("transform", event.transform );
+            graph.attr("transform", event.transform );
             d3.select("svg").attr("cursor", "grabbing");
         })
         .on("end", () => { 
             d3.select("svg").attr("cursor", "default");
         })
 );
+
+drawChart(selection, data[1]);
 
 // Use elliptical arc path segments to doubly-encode directionality.
 function tick() {
@@ -215,64 +224,79 @@ function dragended(event, d) {
 
 // Update interface
 d3.select("#dateSelect").on("click", (event,d)=> {
-    d3.select("#legend").attr("src", "img/legend_date.png");
     updateSelection("date");
+    drawChart("date", caseResult);
 });
 d3.select("#ageSelect").on("click", (event,d)=> {
-    d3.select("#legend").attr("src", "img/legend_age.png");
     updateSelection("age");
+    drawChart("age", caseResult);
 });
 d3.select("#genderSelect").on("click", (event,d)=> {
-    d3.select("#legend").attr("src", "img/legend_gender.png");
     updateSelection("gender");
+    drawChart("gender", caseResult);
 });
 d3.select("#vaccineSelect").on("click", (event,d)=> {
-    d3.select("#legend").attr("src", "img/legend_vaccinated.png");
-    updateSelection("vaccine");
+    updateSelection("vaccinated");
+    drawChart("vaccinated", caseResult);
 });
 d3.select("#asymptomaticSelect").on("click", (event,d)=> {
-    d3.select("#legend").attr("src", "img/legend_asymptomatic.png");
     updateSelection("asymptomatic");
+    drawChart("asymptomatic", caseResult);
+});
+
+d3.select("input[type=search]").on("search", (event, d)=> {
+    caseResult = data[1];
+    searchString = "";
+
+    d3.selectAll(".node")
+    .classed("clicked", false);
+
+    drawChart(selection, caseResult);
 });
 
 d3.select("#searchSubmit").on("click", (event,d)=> {
-    let caseId = d3.select("#searchField").node().value;
-    let regex = new RegExp(caseId.toLowerCase());
+    searchString = d3.select("#searchField").node().value;
 
-    let caseResult = data[1].filter(d => regex.test(d.id) || regex.test(d.occupation.toLowerCase()) || regex.test(d.organization.toLowerCase()));
-    
-    if (caseResult.length > 0) {
+    if (searchString != "") {
+        let regex = new RegExp(searchString.toLowerCase());
+
+        caseResult = data[1].filter(d => d.bigcluster != true && regex.test(d.id) || regex.test(d.occupation.toLowerCase()) || regex.test(d.organization.toLowerCase()));
         
-        d3.selectAll(".node")
-        .classed("clicked", false);
-        
-        caseResult.forEach(e => {
-            d3.select("#case_" + e.id)
-                .classed("clicked", true);
-        })
+        if (caseResult.length > 0) {
+            
+            d3.selectAll(".node")
+            .classed("clicked", false);
+            
+            caseResult.forEach(e => {
+                d3.select("#case_" + e.id)
+                    .classed("clicked", true);
+            })
 
-        if (caseResult.length == 1) {
-            d3.select("#case").text(caseResult[0].id);
-            d3.select("#age").text(caseResult[0].age);
-            d3.select("#gender").text(caseResult[0].gender);
-            d3.select("#occupation").text(caseResult[0].occupation);
-            d3.select("#organization").text(caseResult[0].organization);
-            d3.select("#date_recorded").text( () => {
-                if (caseResult.date == "-") return "-";
-                else return d3.timeFormat("%d %b %Y, %a")(dateFormat(caseResult[0].date));
-            });
-            d3.select("#vaccinated").text(caseResult[0].vaccinated);
+            drawChart(selection, caseResult);
 
-            selectedCase = caseResult[0];
+            if (caseResult.length == 1) {
+                d3.select("#case").text(caseResult[0].id);
+                d3.select("#age").text(caseResult[0].age);
+                d3.select("#gender").text(caseResult[0].gender);
+                d3.select("#occupation").text(caseResult[0].occupation);
+                d3.select("#organization").text(caseResult[0].organization);
+                d3.select("#date_recorded").text( () => {
+                    if (caseResult.date == "-") return "-";
+                    else return d3.timeFormat("%d %b %Y, %a")(dateFormat(caseResult[0].date));
+                });
+                d3.select("#vaccinated").text(caseResult[0].vaccinated);
+
+                selectedCase = caseResult[0];
+            } else {
+                selectedCase = {"id": "-", "age": "-", "gender": "-", "nationality": "-", "occupation": "-", "organization": "-", "date": "-", "vaccinated": "-"};
+            }
+            
         } else {
-            selectedCase = {"id": "-", "age": "-", "gender": "-", "nationality": "-", "occupation": "-", "organization": "-", "date": "-", "vaccinated": "-"};
+            alert("String: " + searchString + " not found");
+            caseResult = data[1];
         }
-        
-    } else {
-        alert("String: " + caseId + " not found");
     }
 });
-
 
 function updateSelection(category) {
   selection = category;
@@ -286,13 +310,152 @@ function updateSelection(category) {
             return ageScale(d.age);
         } else if (selection == "gender") {
             return genderScale(d.gender);
-        } else if (selection == "vaccine") {
-            return vaccineScale(d.vaccinated);
+        } else if (selection == "vaccinated") {
+            return vaccinatedScale(d.vaccinated);
         } else if (selection == "asymptomatic") {
             return asymptomaticScale(d.asymptomatic);
         }
     });
 }
 
+/* Histogram summary chart */
+function drawChart(category, data) {
+    selection = category;
+    d3.select("svg#summarychart g").remove();
+
+    let chart = {
+            width: 400,
+            height: 180,
+            margin: { left: 40, top: 30, right:20, bottom: 40 }
+        };
+
+    let summaryChart = d3.select("svg#summarychart")
+        .attr("viewBox", "0 0 " + chart.width + " " + chart.height)
+        .append("g")
+        .attr("transform", "translate(" + chart.margin.left + ", " + chart.margin.top + " )");
+
+    // Remove all the extra big cluster nodes needed for graph visualization
+    let summaryData = data.filter(d => d.bigcluster != true);
+    
+        if (selection == "date") {
+            summaryData = summaryData.filter(d => dateFormat(d.date) >= dateFormat("16/05/2021"));
+            summaryData = _.entries(_.countBy(summaryData, d => d.date));
+        } else if (selection == "age") {
+            summaryData = _.entries(_.countBy(summaryData, d => Math.floor(d.age / 10))).map(d => [d[0] *10, d[1]]);
+        } else if (selection == "gender") {
+            summaryData = _.entries(_.countBy(summaryData, d => d.gender));
+        } else if (selection == "vaccinated") {
+            summaryData = _.entries(_.countBy(summaryData, d => d.vaccinated));
+        } else if (selection == "asymptomatic") {
+            summaryData = _.entries(_.countBy(summaryData, d => d.asymptomatic))
+                            .map(d => {if (d[0] == "undefined") return ["-", d[1]]; else return [d[0], d[1]]});
+        }
+
+    let xScale = null;
+        if (selection == "date") {
+            xScale = d3.scaleLinear()
+                .domain([dateFormat("16/05/2021"), dateFormat("14/06/2021")])
+                .range([0, chart.width - chart.margin.left - chart.margin.right]);
+        } else if (selection == "age") {
+            xScale = d3.scaleLinear()
+                .domain([0, 100])
+                .range([0, chart.width - chart.margin.left - chart.margin.right]);
+        } else if (selection == "gender") {
+            xScale = d3.scaleBand()
+                .domain(["male", "female"])
+                .rangeRound([0, chart.width - chart.margin.left - chart.margin.right])
+                .padding(0.1);
+        } else if (selection == "vaccinated") {
+            xScale = d3.scaleBand()
+                .domain(["-", "partial (1 dose)", "yes (2 doses)"])
+                .rangeRound([0, chart.width - chart.margin.left - chart.margin.right])
+                .padding(0.1);
+        } else if (selection == "asymptomatic") {
+            xScale = d3.scaleBand()
+                .domain(["-", "yes"])
+                .rangeRound([0, chart.width - chart.margin.left - chart.margin.right])
+                .padding(0.1);
+        }
+
+    let yScale = d3.scaleLinear()
+        .domain([0, d3.max(summaryData.map(d => d[1]))])
+        .range([chart.height - chart.margin.top - chart.margin.bottom, 0]);
+
+    if (selection == "date") {
+        summaryChart
+            .append("g")
+            .attr("class", "axis axis-x")
+            .attr("transform", "translate(0, " + (chart.height - chart.margin.bottom - chart.margin.top) + ")")
+            .call(d3.axisBottom(xScale).ticks(5).tickFormat(d3.timeFormat("%d %b")));
+    } else if (selection == "age" || selection == "gender" || selection == "vaccinated" || selection == "asymptomatic") {
+        summaryChart
+            .append("g")
+            .attr("class", "axis axis-x")
+            .attr("transform", "translate(0, " + (chart.height - chart.margin.bottom - chart.margin.top) + ")")
+            .call(d3.axisBottom(xScale));
+    }
+
+    summaryChart
+        .append("g")
+        .attr("class", "axis axis-y")
+        .call(d3.axisLeft(yScale).ticks(5));
+        
+
+    let chartSearchText = (searchString == "") ? "all cases" : "search: " + searchString;
+
+    summaryChart
+        .append("text")
+        .attr("transform", "translate(" + ((chart.width - chart.margin.right - chart.margin.left)/2) + " , -10)")
+        .style("text-anchor", "middle")
+        .style("font-size", "20px")
+        .style("font-weight", "bold")
+        .text(selection.charAt(0).toUpperCase() + selection.slice(1) + " (" +  chartSearchText + ")");
+
+    if (selection == "date") {
+        summaryChart.selectAll("rect")
+            .data(summaryData)
+            .enter()
+            .append("rect")
+            .attr("x", d => xScale(dateFormat(d[0])) + 1)
+            .attr("width", 10)
+            .attr("y", d => yScale(d[1]))
+            .attr("height", d => yScale(0) - yScale(d[1]))
+            .attr("stroke", "black")
+            .attr("stroke-width", 0.5)
+            .attr("fill", d => dateScale(dateFormat(d[0])));
+    }else if (selection == "age") {
+        summaryChart.selectAll("rect")
+            .data(summaryData)
+            .enter()
+            .append("rect")
+            .attr("x", d => xScale(d[0]) + 1)
+            .attr("width", 30)
+            .attr("y", d => yScale(d[1]))
+            .attr("height", d => yScale(0) - yScale(d[1]))
+            .attr("stroke", "black")
+            .attr("stroke-width", 0.5)
+            .attr("fill", d => ageScale(d[0]));
+    } else if (selection == "gender" || selection == "vaccinated"|| selection == "asymptomatic") {
+        summaryChart.selectAll("rect")
+            .data(summaryData)
+            .enter()
+            .append("rect")
+            .attr("x", d => xScale(d[0]))
+            .attr("width", xScale.bandwidth())
+            .attr("y", d => yScale(d[1]))
+            .attr("height", d => yScale(0) - yScale(d[1]))
+            .attr("stroke", "black")
+            .attr("stroke-width", 0.5)
+            .attr("fill", d => { 
+                if (selection == "gender") {
+                    return genderScale(d[0]);
+                } else if (selection == "vaccinated") {
+                    return vaccinatedScale(d[0]);
+                } else if (selection == "asymptomatic") {
+                    return asymptomaticScale(d[0]);
+                }
+            });
+    }
+}
 
 }); //end Promise
